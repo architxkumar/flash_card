59724a23603a31c145f31567ecf370a22a68d5d7
Archit Kumar <architxkumar@proton.me>
2025-10-19 14:36:53 +0530

    feat: add prototype version with barebone UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,112 @@
+import 'dart:collection';
+
+import 'package:flash_card/quiz.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const FlashcardApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FlashcardApp extends StatelessWidget {
+  const FlashcardApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      title: 'Flash Card',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flash Card'),
+        ),
+        body: Center(
+          child: QuizScreenBody(),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class QuizScreenBody extends StatefulWidget {
+  const QuizScreenBody({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<QuizScreenBody> createState() => _QuizScreenBodyState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _QuizScreenBodyState extends State<QuizScreenBody> {
+  final List<QuizQuestion> _quizQuestions = UnmodifiableListView(quizQuestions);
+  int _currentQuestionIndex = 0;
+  bool _showAnswer = false;
 
-  void _incrementCounter() {
+  void onToggleAnswerVisibility() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _showAnswer = !_showAnswer;
+    });
+  }
+
+  void _onPreviousQuestion() {
+    setState(() {
+      if (_currentQuestionIndex > 0) {
+        _currentQuestionIndex--;
+        _showAnswer = false;
+      }
+    });
+  }
+
+  void _onNextQuestion() {
+    setState(() {
+      if (_currentQuestionIndex < _quizQuestions.length - 1) {
+        _currentQuestionIndex++;
+        _showAnswer = false;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+    bool _isNextButtonDisabled = _currentQuestionIndex >= _quizQuestions.length - 1;
+    bool _isPreviousButtonDisabled = _currentQuestionIndex <= 0;
+    return Column(
+      spacing: 8.0,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          'Question ${_currentQuestionIndex + 1} of ${_quizQuestions.length}',
+          style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+        ),
+        const SizedBox(height: 20.0),
+        Text(
+          _quizQuestions[_currentQuestionIndex].question,
+          style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 20.0),
+        if (_showAnswer)
+          Text(
+            _quizQuestions[_currentQuestionIndex].answer,
+            style: const TextStyle(fontSize: 20.0),
+            textAlign: TextAlign.center,
+          ),
+        const SizedBox(height: 20.0),
+        ElevatedButton(
+          onPressed: onToggleAnswerVisibility,
+          child: Text(_showAnswer ? 'Hide Answer' : 'Show Answer'),
+        ),
+        const SizedBox(height: 20.0),
+        Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            ElevatedButton(
+              onPressed: _isPreviousButtonDisabled ? null : _onPreviousQuestion,
+              child: const Text('Previous'),
+            ),
+            const SizedBox(width: 20.0),
+            ElevatedButton(
+              onPressed: _isNextButtonDisabled ? null : _onNextQuestion,
+              child: const Text('Next'),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
     );
   }
 }
